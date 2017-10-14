12aaa6f4e0925fdd520996472267b26aa89f7fd9
Dennis Burger <dennis@dutchwebworks.nl>
2017-10-14 16:56:26 +0200

    Refactored so component has data object.

--- js/app.js
+++ js/app.js
@@ -10,18 +10,11 @@ var bus = new Vue();
 
 Vue.component("app-movie-product", {
 	template: "#vue-app-movie-product",
-	props: ['movie']	
-});
-
-// ---------------------------------------------
-// Main instance
-// ---------------------------------------------
-
-new Vue({
-	el: "#app",
-	data: {
-		productsJsonUrl: "json/products.json",
-		movies: []
+	data: function() {
+		return {
+			productsJsonUrl: "json/products.json",
+			movies: []
+		}
 	},
 	mounted: function(){
 		this.loadMovies(this.productsJsonUrl);
@@ -30,6 +23,15 @@ new Vue({
 		loadMovies: function(url) {
 			axios.get(url).then(response => this.movies = response.data);
 		}
-	}
+	}	
+});
+
+// ---------------------------------------------
+// Main instance
+// ---------------------------------------------
+
+new Vue({
+	el: "#app",
+	
 });
 
