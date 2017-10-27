737e723f55a547f034e8fe9bde6a0b880b3ffc40
Dennis Burger <dennis.burger@poort80.nl>
2017-10-27 10:10:53 +0200

    Refactored some HTML, Scss and JavaScript: proper order and arrangement of blocks.

--- js/app.js
+++ js/app.js
@@ -8,156 +8,6 @@ var bus = new Vue();
 // Components
 // ---------------------------------------------
 
-Vue.component("app-movie-products", {
-	template: "#vue-app-movie-products",
-	props: ['filteredMovies'],
-	data: function() {
-		return {
-			filterByGenre: "",
-		}
-	},
-	methods: {
-		addToCart: function(event, item) {
-			bus.$emit("addToCart", item);
-		},
-		filterMovieList: function(genre) {
-			bus.$emit("filterGenre", genre);
-		}
-	}	
-});
-
-Vue.component("app-pagination", {
-	template: "#vue-app-pagination",
-	props: ['pagination'],
-	data: function() {
-		return {
-			showPagination: true		
-		}
-	},
-	created: function() {
-		var self = this;
-	},
-	methods: {
-		paginatedMovies: function(index) {
-			this.pagination.currentPage = index;
-			this.pagination.start = this.pagination.pageIncrementCounter[index];
-		},
-		prevPage: function() {
-			this.pagination.currentPage--;			
-
-			if(this.pagination.currentPage < 0) {
-				this.pagination.currentPage = 0;
-			}
-
-			this.pagination.start = this.pagination.pageIncrementCounter[this.pagination.currentPage];
-		},
-		nextPage: function(){
-			if(this.pagination.currentPage < (this.pagination.pages -1)) {
-				this.pagination.currentPage++;	
-				this.pagination.start = this.pagination.pageIncrementCounter[this.pagination.currentPage];
-			}
-		}
-	}
-});
-
-Vue.component("app-cart", {
-	template: "#vue-app-cart",
-	data: function() {
-		return {
-			cartItems: [],
-			total: 0
-		}
-	},
-	created: function(){
-		bus.$on("addToCart", this.addToCart);
-	},
-	methods: {
-		addToCart: function(item) {
-			this.cartItems.push(item);
-			this.total += parseInt(item.price);
-		},
-		removeFromCart: function(event, item, key) {
-			this.total -= parseInt(item.price);
-			this.cartItems.splice(key, 1);
-		},
-		emptyCart: function(){
-			var confirmEmpty = confirm("Remove all " + this.cartItems.length + " items?");
-
-			if(confirmEmpty == true) {
-				this.cartItems = [];
-				this.total = 0;				
-			}
-		},
-		checkout: function() {
-			alert("Pay €" + this.total + " for " + this.cartItems.length + " items?");
-		}
-	},
-});
-
-Vue.component("app-genre", {
-	template: "#vue-app-genre",
-	props: ['total'],
-	data: function() {
-		return {
-			genres: [],
-			selected: ''
-		}
-	},
-	created: function() {
-		var self = this;
-
-		bus.$on("generateGenres", this.generateGenres);
-		bus.$on("setGenre", function(genre){
-			self.selected = genre;
-		});
-	},
-	methods: {
-		generateGenres: function(data) {
-			var genreList = [];
-
-			for(i = 0, j = data.length; i < j; i++){
-				genreList.push({
-					genre: data[i].genre,
-					count: 1
-				});
-			}
-
-			var map = genreList.reduce(function(map, value) {
-				var genre = value.genre;
-				var count = +value.count;
-				map[genre] = (map[genre] || 0) + count
-					return map
-				}, 
-			{});
-
-			var array = Object.keys(map).map(function(genre) {
-				return {
-					genre: genre,
-					count: map[genre]
-				}
-			});
-
-			array.sort(function(a, b){
-				return a.genre > b.genre;
-			});
-			
-			this.genres = array;
-		},
-		filterMovieList: function(element) {
-			bus.$emit("filterGenre", element.currentTarget.value);
-		}
-	}
-});
-
-Vue.component("app-welcome", {
-	template: "#vue-app-welcome",
-	methods: {
-		switchComponent: function(name) {
-			bus.$emit("switchComponent", name);
-		}
-	}
-});
-
 Vue.component("app-shop", {
 	template: "#vue-app-shop",
 	data: function() {
@@ -224,6 +74,147 @@ Vue.component("app-shop", {
 	}
 });
 
+Vue.component("app-genre", {
+	template: "#vue-app-genre",
+	props: ['total'],
+	data: function() {
+		return {
+			genres: [],
+			selected: ''
+		}
+	},
+	created: function() {
+		var self = this;
+
+		bus.$on("generateGenres", this.generateGenres);
+		bus.$on("setGenre", function(genre){
+			self.selected = genre;
+		});
+	},
+	methods: {
+		generateGenres: function(data) {
+			var genreList = [];
+
+			for(i = 0, j = data.length; i < j; i++){
+				genreList.push({
+					genre: data[i].genre,
+					count: 1
+				});
+			}
+
+			var map = genreList.reduce(function(map, value) {
+				var genre = value.genre;
+				var count = +value.count;
+				map[genre] = (map[genre] || 0) + count
+					return map
+				}, 
+			{});
+
+			var array = Object.keys(map).map(function(genre) {
+				return {
+					genre: genre,
+					count: map[genre]
+				}
+			});
+
+			array.sort(function(a, b){
+				return a.genre > b.genre;
+			});
+			
+			this.genres = array;
+		},
+		filterMovieList: function(element) {
+			bus.$emit("filterGenre", element.currentTarget.value);
+		}
+	}
+});
+
+Vue.component("app-pagination", {
+	template: "#vue-app-pagination",
+	props: ['pagination'],
+	data: function() {
+		return {
+			showPagination: true		
+		}
+	},
+	created: function() {
+		var self = this;
+	},
+	methods: {
+		paginatedMovies: function(index) {
+			this.pagination.currentPage = index;
+			this.pagination.start = this.pagination.pageIncrementCounter[index];
+		},
+		prevPage: function() {
+			this.pagination.currentPage--;			
+
+			if(this.pagination.currentPage < 0) {
+				this.pagination.currentPage = 0;
+			}
+
+			this.pagination.start = this.pagination.pageIncrementCounter[this.pagination.currentPage];
+		},
+		nextPage: function(){
+			if(this.pagination.currentPage < (this.pagination.pages -1)) {
+				this.pagination.currentPage++;	
+				this.pagination.start = this.pagination.pageIncrementCounter[this.pagination.currentPage];
+			}
+		}
+	}
+});
+
+Vue.component("app-movie-products", {
+	template: "#vue-app-movie-products",
+	props: ['filteredMovies'],
+	data: function() {
+		return {
+			filterByGenre: "",
+		}
+	},
+	methods: {
+		addToCart: function(event, item) {
+			bus.$emit("addToCart", item);
+		},
+		filterMovieList: function(genre) {
+			bus.$emit("filterGenre", genre);
+		}
+	}	
+});
+
+Vue.component("app-cart", {
+	template: "#vue-app-cart",
+	data: function() {
+		return {
+			cartItems: [],
+			total: 0
+		}
+	},
+	created: function(){
+		bus.$on("addToCart", this.addToCart);
+	},
+	methods: {
+		addToCart: function(item) {
+			this.cartItems.push(item);
+			this.total += parseInt(item.price);
+		},
+		removeFromCart: function(event, item, key) {
+			this.total -= parseInt(item.price);
+			this.cartItems.splice(key, 1);
+		},
+		emptyCart: function(){
+			var confirmEmpty = confirm("Remove all " + this.cartItems.length + " items?");
+
+			if(confirmEmpty == true) {
+				this.cartItems = [];
+				this.total = 0;				
+			}
+		},
+		checkout: function() {
+			alert("Pay €" + this.total + " for " + this.cartItems.length + " items?");
+		}
+	},
+});
+
 Vue.component("app-checkout", {
 	template: "#vue-app-checkout",
 	data: function() {
@@ -243,21 +234,13 @@ Vue.component("app-checkout", {
 });
 
 // ---------------------------------------------
-// Globlal filters
-// ---------------------------------------------
-
-Vue.filter("capitalizeFirstLetter", function(string){
-	return string.charAt(0).toUpperCase() + string.slice(1);
-});
-
-// ---------------------------------------------
 // Main instance
 // ---------------------------------------------
 
 new Vue({
 	el: "#app",
 	data: {
-		selectedComponent: "app-welcome",
+		selectedComponent: "app-shop",
 	},
 	created: function() {
 		var self = this;
@@ -265,4 +248,12 @@ new Vue({
 			self.selectedComponent = name;
 		});
 	}
+});
+
+// ---------------------------------------------
+// Globlal filters
+// ---------------------------------------------
+
+Vue.filter("capitalizeFirstLetter", function(string){
+	return string.charAt(0).toUpperCase() + string.slice(1);
 });
