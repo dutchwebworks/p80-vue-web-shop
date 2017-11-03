71caca12a7e912903fa2c752842c8d5b5151f7d6
Dennis Burger <dennis.burger@poort80.nl>
2017-11-03 14:58:02 +0100

    Moved vee validate instance to the app js file.

--- js/vue-app.js
+++ js/vue-app.js
@@ -334,3 +334,9 @@ new Vue({
 Vue.filter("capitalizeFirstLetter", function(string){
 	return string.charAt(0).toUpperCase() + string.slice(1);
 });
+
+// ---------------------------------------------
+// Vee Validate
+// ---------------------------------------------
+
+Vue.use(VeeValidate);
